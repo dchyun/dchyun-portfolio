d7c89eef9b86c194b5c2bbe7fa8ea8551bb16325
Dylan Hyun <dcdc97@gmail.com>
2024-09-16 15:38:18 -0400

    project page toc active state style update and ada fix

--- src/scripts/project-detail.js
+++ src/scripts/project-detail.js
@@ -39,16 +39,38 @@ function setStepComplete(step) {
   step.classList.remove('step-active');
   step.classList.remove('step-incomplete');
   step.classList.add('step-complete');
+  removeActiveStepSrText(step);
 }
 
 function setStepActive(step) {
   step.classList.remove('step-complete');
   step.classList.remove('step-incomplete');
   step.classList.add('step-active');
+  addActiveStepSrText(step);
 }
 
 function setStepIncomplete(step) {
   step.classList.remove('step-complete');
   step.classList.remove('step-active');
   step.classList.add('step-incomplete');
+  removeActiveStepSrText(step);
+}
+
+function addActiveStepSrText(step) {
+  let link = step.querySelector('a');
+  const currentSrOnlyText = link.querySelector('.sr-only');
+  if (!currentSrOnlyText) {
+    let srOnlyText = document.createElement('div');
+    srOnlyText.classList.add('sr-only');
+    srOnlyText.innerText = 'active';
+    link.appendChild(srOnlyText);
+  }
+}
+
+function removeActiveStepSrText(step) {
+  let link = step.querySelector('a');
+  let srOnlyText = link.querySelector('.sr-only');
+  if (srOnlyText) {
+    link.removeChild(srOnlyText);
+  }
 }
